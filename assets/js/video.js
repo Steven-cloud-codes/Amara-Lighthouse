ff93645d9439b1858ed756af6cfddf59b4a0ddfa
Your Name <stevenluke959726@gmail.com>
2025-07-30 06:34:55 +0630

    video player added but need to be link with fire-base

---
+++ assets/js/video.js
@@ -0,0 +1,9 @@
+const videoList = [
+  {
+    id: "intro001",
+    title: "Introduction to Ancient Myanmar",
+    description: "An overview of Myanmar's early civilization.",
+    file: "https://drive.google.com/uc?export=preview&id=1fjZKI5jRMfxZHsqr3kUeMD4xPJu8RoDw",
+    source: "https://www.youtube.com/watch?v=example123" // (if you also want to give credit)
+  }
+];
