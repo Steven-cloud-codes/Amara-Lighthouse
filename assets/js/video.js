997d8655d80312a4ba81643912a46338a7411006
Anna <thunshwesinlin@gmail.com>
2025-08-07 21:09:24 +0630

    added vids and carts on shop

--- assets/js/video.js
+++ assets/js/video.js
@@ -5,5 +5,26 @@ const videoList = [
     description: "An overview of Myanmar's early civilization.",
     file: "https://drive.google.com/file/d/1fjZKI5jRMfxZHsqr3kUeMD4xPJu8RoDw/preview",
     source: "https://www.youtube.com/watch?v=example123" // (if you also want to give credit)
+  },
+  {
+    id: "intro002",
+    title: "Mohinga",
+    description: "Rice noodle soup with fish broth, Myanmar’s national dish.",
+    file: "https://drive.google.com/file/d/1IwHdN2ghPDFOz1Vr7WChLriOkPvWJ_65/preview",
+    source: "https://youtu.be/cAR_ckVoqTw?si=TOJVoN4YCjs_N0G1" // (if you also want to give credit)
+  },
+  {
+    id: "intro003",
+    title: "Laphet Thoke",
+    description: "Fermented tea leaf salad mixed with nuts and veggies.",
+    file: "https://drive.google.com/file/d/1-vuH29RBs9pgbpMXMlNe_D6Wr5H6s8ZR/preview",
+    source: "https://youtu.be/huwgevAcNWs?si=bsX6JkyfTr7Qyy2i" // (if you also want to give credit)
+  },
+  {
+    id: "intro004",
+    title: "Shan Noodles",
+    description: "Soft rice noodles with chicken or pork in a light tomato sauce.",
+    file: "https://drive.google.com/file/d/1IwHdN2ghPDFOz1Vr7WChLriOkPvWJ_65/preview",
+    source: "https://youtu.be/otblLjczF44?si=E7YBYtmMI6Tjr8fm" // (if you also want to give credit)
   }
 ];
