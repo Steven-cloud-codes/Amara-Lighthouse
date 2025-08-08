d15f5ba6287180525967fa63c552d23cabc47642
Anna <thunshwesinlin@gmail.com>
2025-08-08 10:58:32 +0630

    added vids in festival

--- assets/js/video.js
+++ assets/js/video.js
@@ -24,8 +24,36 @@ const videoList = [
     id: "intro004",
     title: "Shan Noodles",
     description: "Soft rice noodles with chicken or pork in a light tomato sauce.",
-    file: "https://drive.google.com/file/d/1IwHdN2ghPDFOz1Vr7WChLriOkPvWJ_65/preview",
+    file: "https://drive.google.com/file/d/1q-ZCkJSG8GCFkuYOzWY2vJlqX7hE7Qdb/preview",
     source: "https://youtu.be/otblLjczF44?si=E7YBYtmMI6Tjr8fm" // (if you also want to give credit)
-  }
+  },
+  {
+    id: "intro005",
+    title: "Thingyan Festival",
+    description: "Myanmar New Year water festival, celebrated with water throwing and music.",
+    file: "https://drive.google.com/file/d/1PASsKjErPhieEYUPiIrlZKX8IPqy3Ng9/preview",
+    source: "https://youtu.be/3WOwoJnTsnA?si=AiX2XsIs9-uidKqH" // (if you also want to give credit)
+  },
+  {
+    id: "intro006",
+    title: "Thadingyut Festival",
+    description: "Festival of Lights marking the Buddha’s descent from heaven, with candles and lanterns.",
+    file: "https://drive.google.com/file/d/18piYSVpcWIH8JEsCMttwdZVCVd7phiF6/preview",
+    source: "https://youtu.be/JdA7W8LvT1Y?si=VSt6nJJwQZBZdnah" // (if you also want to give credit)
+  },
+  {
+    id: "intro007",
+    title: "Tazaungdaing Festival",
+    description: "Light festival with hot-air balloons, robes offering, and donations.",
+    file: "https://drive.google.com/file/d/1gK5CBrZAaZ6RJGcNaXIVNV0213xVNSlt/preview",
+    source: "https://youtu.be/xIaXIvbTzwA?si=6fFaxMO-OPX_XbCa" // (if you also want to give credit)
+  },
+  {
+    id: "intro008",
+    title: "Waso Festival",
+    description: "Marks the beginning of Buddhist Lent; monks receive robes and people observe religious practices",
+    file: "https://drive.google.com/file/d/1vhCUAWg1z7a4sDe6AkGpuzt8ToOYwcoi/preview",
+    source: "https://youtu.be/Ke0TOdq2t3A?si=W6ta1N5FgYYd8TxF" // (if you also want to give credit)
+  },
    
 ];
